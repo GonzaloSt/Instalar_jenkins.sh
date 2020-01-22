bc45200d5bdf4acf473e57393961f29d0c5f3100
GonzaloSt <gonzalostete@hotmail.com>
2020-01-22 14:01:35 -0300

    update token
    se agrego el cateo del token de jenkins

--- instalar_jenkins.sh
+++ instalar_jenkins.sh
@@ -4,3 +4,4 @@ wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt update
 sudo apt install -y jenkins
+cat /var/lib/jenkins/secrets/initialAdminPassword
